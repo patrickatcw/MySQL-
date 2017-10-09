e00e5c324ec77d94c7b6d07883257c345e56071f
patrickatcw <pj8392@yahoo.com>
2017-10-09 09:02:09 -0400

    updated connection file

--- bamazonConnection.js
+++ bamazonConnection.js
@@ -18,3 +18,10 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
+
+function selectAll() {
+  connection.query("select * from products", function(err, results){
+    if(err){throw err;}
+    console.log(results);
+  });
+}
